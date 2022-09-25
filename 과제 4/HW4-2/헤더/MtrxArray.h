2ad29bd826ba21b55be8cc39d7585ff476d84507
머홍 <101238645+Wa-MeoHong@users.noreply.github.com>
2022-09-26 02:27:10 +0900

    과제 4
    과제 4

--- 과제 4/HW4-2/헤더/MtrxArray.h
+++ 과제 4/HW4-2/헤더/MtrxArray.h
@@ -6,17 +6,17 @@
 
 using namespace std;
 
-class MtrxArray
+class MtrxArray						// 행렬 배열 클래스
 {
 public:
-	MtrxArray(int array_size);
-	~MtrxArray();
-	Mtrx& operator[](int);
+	MtrxArray(int array_size);		// 행렬 배열 생성자 
+	~MtrxArray();					// 행렬 배열 소멸자
+	Mtrx& operator[](int);			// 행렬 반환
 
 private:
-	Mtrx* pMtrx;
-	int mtrxArraySIZE;
-	bool isValidIndex(int index);
+	Mtrx* pMtrx;					// 행렬 배열 담는 포인터
+	int mtrxArraySIZE;				// 배열의 크기
+	bool isValidIndex(int index);	// 행렬 인덱스 번호 유효성 검사
 };
 
 #endif
